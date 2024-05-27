9f32ba7aa0d251431d85335da82297f4323f1c51
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-05-27 03:48:51 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+//JavaScript Kodları
+
+function scrollToElement(elementSelector, instance = 0) {
+
+    if (elementSelector.length > instance) {
+        elements[instance].scrollIntoView({ behavior: 'smooth' });
+    }
+
+}
+
+
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+link1.addEventListener('click', () => {
+    scrollToElement('.header');
+});
+
+link2.addEventListener('click', () => {
+    scrollToElement('.header');
+});
+
+link2.addEventListener('click', () => {
+    scrollToElement('.header', 1);
+
+});
+
+link3.addEventListener('click', () => {
+    scrollToElement('.column');
+})
